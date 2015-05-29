83256a34b4e1551c58430b97c3192f745c4edd5c
mattjmorrison <mattjmorrison@mattjmorrison.com>
2015-05-29 12:01:03 -0500

    Debugging: Management command to display s3 static settings

---
+++ demo/managements/commands/display_s3_settings.py
@@ -0,0 +1,11 @@
+from django.core.management.base import BaseCommand
+from django.conf import settings
+
+
+class Command(BaseCommand):
+
+    def handle(self):
+        print 'STATIC_URL = {}'.format(settings.STATIC_URL)
+        print 'AWS_ACCESS_KEY_ID = {}'.format(settings.AWS_ACCESS_KEY_ID)
+        print 'AWS_SECRET_ACCESS_KEY = {}'.format(settings.AWS_SECRET_ACCESS_KEY)
+        print 'AWS_STORAGE_BUCKET_NAME = {}'.format(settings.AWS_STORAGE_BUCKET_NAME)
